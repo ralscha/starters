40b4a08c9f986e93b58eca5a121a610163f7137c
Ralph Schaer <ralphschaer@gmail.com>
2021-04-27 15:44:12 +0200

    Upgrade

--- ionicblank/src/zone-flags.ts
+++ ionicblank/src/zone-flags.ts
@@ -2,5 +2,5 @@
  * Prevents Angular change detection from
  * running with certain Web Component callbacks
  */
-// tslint:disable-next-line:no-any
+// eslint-disable-next-line no-underscore-dangle
 (window as any).__Zone_disable_customElements = true;
