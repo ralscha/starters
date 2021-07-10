525d9aa284f2af9307eb51fdc7a0572dbbcc7275
Ralph Schaer <ralphschaer@gmail.com>
2021-07-10 08:49:29 +0200

    Upgrade starter templates

--- ionicblank/src/app/app.module.ts
+++ ionicblank/src/app/app.module.ts
@@ -1,23 +1,18 @@
-import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
 
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(),
-    AppRoutingModule
-  ],
-  providers: [
-    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{provide: RouteReuseStrategy, useClass: IonicRouteStrategy}],
+  bootstrap: [AppComponent],
 })
 export class AppModule {
 }
