84b3280de97200e701d1e318bb422b3bbd64af09
Ralph Schaer <ralphschaer@gmail.com>
2024-10-13 19:23:13 +0200

    Upgrade

--- ionicblank/src/main.ts
+++ ionicblank/src/main.ts
@@ -1,15 +1,9 @@
-import {enableProdMode} from '@angular/core';
 import {bootstrapApplication} from '@angular/platform-browser';
 import {PreloadAllModules, provideRouter, RouteReuseStrategy, withPreloading} from '@angular/router';
 import {IonicRouteStrategy, provideIonicAngular} from '@ionic/angular/standalone';
 
 import {routes} from './app/app.routes';
 import {AppComponent} from './app/app.component';
-import {environment} from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [
